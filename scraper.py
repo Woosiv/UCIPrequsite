c666a76420d37fbaaa6498bf0a572d3db37b2148
Woosiv <williamhsiehoct@gmail.com>
2021-06-30 00:19:54 -0700

    Rebuilt using BeautifulSoup built-in methods

--- scraper.py
+++ scraper.py
@@ -9,9 +9,44 @@ siteHtml = site.read()
 soup = BeautifulSoup(siteHtml, 'html.parser')
 # Course listing are under the sc_courselist class tag
 require = soup.find(class_ = 'sc_courselist')
+require2 = soup.find_all(class_ = ['bubblelink code', 'courselistcomment'])
 #print(require.get_text())
 rString = require.get_text().replace(u'\xa0', u' ')
+rString2 = require.get_text().replace(u'\xa0', u' ')
 
+# Using Beautiful Soup to get the requirements instead of regex 
+def convert_requirement (req : BeautifulSoup) -> dict:
+    res = {}
+    last_req = ''
+    course = []
+    last_depart = ''
+    for div in req:
+        text = div.get_text().replace(u'\xa0', u' ')
+        if 'bubblelink' in div['class']:
+            try:
+                last_depart = re.match(r"(?:[A-Z&]+[0-9]?\s?[A-Z&]+)+", text)[0]
+                course.append(text)
+            except:
+                course.append(last_depart + text)
+            print('Class')
+            if div.parent.has_attr('class') and 'orclass' in div.parent['class']:
+                temp = []
+                temp.append(course)
+                course = temp
+        elif 'courselistcomment' in div['class']:
+            print('Requirement')
+            if last_req:
+                res[last_req] = course
+                last_req = ''
+                course = []
+            last_req = div.get_text()
+            print(last_req)
+        print()
+    print(res)
+
+convert_requirement(require2)
+
+assert False
 # Helper regex that removes title descriptors as well as white spaces
 def stripTitle(target : str) -> str:
     return re.sub(r"\(?[A-Za-z][a-z]+:?\)?", "", target).strip()
